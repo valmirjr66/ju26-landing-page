14183c279260a0d30a04f2811ec37657914ccfd6
Valmir Júnior <valmirgmj@gmail.com>
2026-07-18 01:06:19 -0300

    simplify code

--- src/components/shared/dialog.tsx
+++ src/components/shared/dialog.tsx
@@ -54,30 +54,6 @@ function Dialog({
   );
 }
 
-function DialogPortal({
-  ...props
-}: React.ComponentProps<typeof DialogPrimitive.Portal>) {
-  return <DialogPrimitive.Portal data-slot="dialog-portal" {...props} />;
-}
-
-function DialogOverlay({
-  className,
-  ...props
-}: React.ComponentProps<typeof DialogPrimitive.Overlay>) {
-  return (
-    <DialogPrimitive.Overlay
-      data-slot="dialog-overlay"
-      className={cn(
-        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50",
-        className
-      )}
-      {...props}
-    />
-  );
-}
-
-DialogOverlay.displayName = "DialogOverlay";
-
 function DialogContent({
   className,
   children,
@@ -108,8 +84,13 @@ function DialogContent({
   );
 
   return (
-    <DialogPortal data-slot="dialog-portal">
-      <DialogOverlay />
+    <DialogPrimitive.Portal data-slot="dialog-portal">
+      <DialogPrimitive.Overlay
+        data-slot="dialog-overlay"
+        className={cn(
+          "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50"
+        )}
+      />
       <DialogPrimitive.Content
         data-slot="dialog-content"
         className={cn(
@@ -130,7 +111,7 @@ function DialogContent({
           </DialogPrimitive.Close>
         )}
       </DialogPrimitive.Content>
-    </DialogPortal>
+    </DialogPrimitive.Portal>
   );
 }
 
