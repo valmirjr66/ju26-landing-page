69d49873e45f4faa67bbf5db42a6768cdd65e35d
Valmir Júnior <valmirgmj@gmail.com>
2026-07-18 01:57:55 -0300

    enhance who-am-i prototype

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -1,4 +1,5 @@
 import backgroundVideo from "@/assets/background_media/loop_video_2.mp4";
+import juPhoto from "@/assets/photos/ju_2.png";
 import {
   Dialog,
   DialogContent,
@@ -6,6 +7,34 @@ import {
   DialogTitle,
 } from "@/components/shared/dialog";
 import { useState } from "react";
+
+function TimelineItem({
+  img,
+  imgAlt,
+  text,
+  reverseOrder = false,
+  suppressMarginBottom = false,
+}: {
+  img: string;
+  imgAlt: string;
+  text: string;
+  reverseOrder?: boolean;
+  suppressMarginBottom?: boolean;
+}) {
+  return (
+    <div
+      className={`${suppressMarginBottom || "mb-10"} flex ${reverseOrder ? "flex-row-reverse" : "flex-row"}`}
+    >
+      <img
+        src={img}
+        alt={imgAlt}
+        style={{ width: 150 }}
+        className={reverseOrder ? "ml-5" : "mr-5"}
+      />
+      <span className="text-justify">{text}</span>
+    </div>
+  );
+}
 
 export default function WhoAmI() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -73,11 +102,52 @@ export default function WhoAmI() {
           <DialogHeader>
             <DialogTitle className="font-arcade">SAIBA MAIS</DialogTitle>
           </DialogHeader>
-          <div className="py-4">
-            <p className="font-retropix text-black">
-              Conteúdo adicional sobre MC Ju será adicionado aqui. Esta é uma
-              seção de protótipo.
-            </p>
+          <div
+            className="py-4 overflow-y-auto font-retropix text-black"
+            style={{ maxHeight: 400 }}
+          >
+            <TimelineItem
+              img={juPhoto}
+              imgAlt="Ju photo 1"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit."
+            />
+
+            <TimelineItem
+              reverseOrder
+              img={juPhoto}
+              imgAlt="Ju photo 2"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit."
+            />
+
+            <TimelineItem
+              img={juPhoto}
+              imgAlt="Ju photo 3"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit."
+            />
+
+            <TimelineItem
+              reverseOrder
+              suppressMarginBottom
+              img={juPhoto}
+              imgAlt="Ju photo 4"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
+                amet, consectetur adipiscing elit."
+            />
           </div>
         </DialogContent>
       </Dialog>
