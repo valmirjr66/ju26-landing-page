c9a5fd855b0b5f8ca134385e6bd6f53e1d991474
Valmir Júnior <valmirgmj@gmail.com>
2026-07-21 01:55:05 -0300

    update WhoAmI section

--- src/components/WhoAmI.tsx
+++ src/components/WhoAmI.tsx
@@ -1,5 +1,6 @@
 import backgroundVideo from "@/assets/background_media/loop_video_2.mp4";
-import juPhoto from "@/assets/photos/ju_2.png";
+import timelinePhoto1 from "@/assets/photos/timeline_1.png";
+import timelinePhoto2 from "@/assets/photos/timeline_2.png";
 import {
   Dialog,
   DialogContent,
@@ -70,20 +71,26 @@ export default function WhoAmI() {
             <h3 className="text-center mb-6 text-black">MAIS QUE O VULGO</h3>
 
             <p className="font-retropix text-black text-justify mb-8 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
+              Eu sou a Ju. Estou há 9 anos na luta coletiva, batalhando por
+              justiça social e climática.
               <br />
               <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
+              Iniciei minha trajetória na igreja evangélica, onde aprendi sobre
+              amor e coletividade. Apesar da contradição dessa instituição,
+              ainda hoje o Jesus negro, da periferia de Nazaré, permanece
+              guiando meus passos. Na escola tive professores referências que me
+              ensinaram a ser crítica sobre a desigualdade brasileira.
+              <br />
+              <br />
+              Mas foi na faculdade de medicina que iniciei minha trajetória como
+              sujeito político. Fiz parte da DENEM, do Diretório Acadêmico e
+              nessa jornada, também conheci a Juventude Manifesta, que me
+              ensina, me transforma e me transborda ainda hoje.
+              <br />
+              <br />A partir da sede de mudança que a Manifesta me instigou e de
+              mulheres que tornaram-se minhas referências, iniciei minha jornada
+              no Hip Hop, movimento cultural que me trouxe o freestyle como
+              ferramenta para mudar o mundo.
             </p>
 
             <div className="flex justify-end">
@@ -109,28 +116,29 @@ export default function WhoAmI() {
             style={{ maxHeight: 400 }}
           >
             <TimelineItem
-              img={juPhoto}
+              img={timelinePhoto1}
               imgAlt="Ju photo 1"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
+              text="Em 2019 iniciei minha caminhada na DENEM, no movimento estudantil,
+              entidade que me fez entender meu lugar na medicina. Aprendi que saúde
+              vai muito além de bem estar social, mental e físico. Aprendi que saúde
+              é justiça social e que para promovê-la, nosso povo necessita de políticas
+              públicas fortalecidas. Aprendi que nossa atuação dentro do consultório
+              é insuficiente, e, por isso, a mão que cuida, também deve lutar."
             />
 
             <TimelineItem
               reverseOrder
-              img={juPhoto}
+              img={timelinePhoto2}
               imgAlt="Ju photo 2"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
+              text="A pandemia foi um momento desafiador para nosso povo, principalmente
+              com o desgoverno promove do negacionismo, desinformação e irresponsabilidade.
+              Foram, pelo menos, 400 mil lutos evitáveis. E, mesmo exaustas, fizemos o
+              luto virar luta. Ocupamos as ruas pelo Fora Bolsonaro, junto com o movimento
+              estudantil de medicina, utilizando máscaras, distanciamento social e álcool 70%."
             />
 
             <TimelineItem
-              img={juPhoto}
+              img={timelinePhoto2}
               imgAlt="Ju photo 3"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -142,7 +150,7 @@ export default function WhoAmI() {
             <TimelineItem
               reverseOrder
               suppressMarginBottom
-              img={juPhoto}
+              img={timelinePhoto2}
               imgAlt="Ju photo 4"
               text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
