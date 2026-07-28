bb5b6ebd36b235e2d7933b73922ad2ef14fafff4
Valmir Júnior <valmirgmj@gmail.com>
2026-07-28 14:59:42 -0300

    update "who am i" section

--- src/components/WhoAmISection.tsx
+++ src/components/WhoAmISection.tsx
@@ -1,11 +1,10 @@
 import backgroundVideo from "@/assets/background_media/loop_video_2.mp4";
-import timelinePhoto1 from "@/assets/photos/timeline_1.png";
-import timelinePhoto2 from "@/assets/photos/timeline_2.png";
-import timelinePhoto3 from "@/assets/photos/timeline_3.png";
-import timelinePhoto4 from "@/assets/photos/timeline_4.png";
-import timelinePhoto5 from "@/assets/photos/timeline_5.png";
-import timelinePhoto6 from "@/assets/photos/timeline_6.png";
-import timelinePhoto7 from "@/assets/photos/timeline_7.png";
+import timelinePhoto1 from "@/assets/photos/timeline_1.jpg";
+import timelinePhoto2 from "@/assets/photos/timeline_2.jpg";
+import timelinePhoto3 from "@/assets/photos/timeline_3.jpg";
+import timelinePhoto4 from "@/assets/photos/timeline_4.jpg";
+import timelinePhoto5 from "@/assets/photos/timeline_5.jpg";
+import timelinePhoto6 from "@/assets/photos/timeline_6.jpg";
 import {
   Dialog,
   DialogContent,
@@ -80,22 +79,30 @@ export default function WhoAmISection() {
               justiça social e climática.
               <br />
               <br />
-              Iniciei minha trajetória na igreja evangélica, onde aprendi sobre
-              amor e coletividade. Apesar da contradição dessa instituição,
-              ainda hoje o Jesus negro, da periferia de Nazaré, permanece
-              guiando meus passos. Na escola tive professores referências que me
-              ensinaram a ser crítica sobre a desigualdade brasileira.
+              Nasci e cresci em berço evangélico, onde aprendi sobre amor e
+              coletividade. Apesar da contradição da instituição Igreja, ainda
+              hoje o Jesus negro, amoroso, da periferia de Nazaré, permanece
+              guiando meus passos. Já interpretando o mundo pela ótica do amor,
+              tive referências intelectuais que me ensinaram a ser crítica sobre
+              a realidade.
               <br />
               <br />
-              Mas foi na faculdade de medicina que iniciei minha trajetória como
-              sujeito político. Fiz parte da DENEM, do Diretório Acadêmico e
-              nessa jornada, também conheci a Juventude Manifesta, que me
-              ensina, me transforma e me transborda ainda hoje.
+              Apesar disso, faltava movimento. Somente fazendo medicina na UIT,
+              há quase 10 anos atrás, começo a me organizar em coletivos. Fiz
+              parte da DENEM, do DALASS e dos coletivos de mulheres Nise da
+              Silveira e LGBTQIAPN+ Indianara Siqueira.
               <br />
-              <br />A partir da sede de mudança que a Manifesta me instigou e de
-              mulheres que tornaram-se minhas referências, iniciei minha jornada
-              no Hip Hop, movimento cultural que me trouxe o freestyle como
-              ferramenta para mudar o mundo.
+              <br />
+              Mas foi um dos poucos mediciners negros da UIT que mudou o rumo da
+              minha vida. Ele me convidou pra uma reunião do PSOL e me aplicou
+              no rap. Assim, me encontrei politicamente no PSOL e na Juventude
+              Manifesta, onde me forjo liderança na prática.
+              <br />
+              <br />
+              Já amando o rap e querendo conhecer outros jovens na minha cidade,
+              descobri o Culture, um bar que reunia todo tipo de galera.
+              Inclusive as mina do hip hop. E foi assim, há 4 anos atrás, que me
+              tornei artista e produtora cultural.
             </p>
 
             <div className="flex justify-end">
@@ -122,77 +129,74 @@ export default function WhoAmISection() {
           >
             <TimelineItem
               img={timelinePhoto1}
-              imgAlt="Ju photo 1"
-              text="Em 2019 iniciei minha caminhada na DENEM, no movimento estudantil,
+              imgAlt="Ju construindo com a DENEM (Direção Executiva Nacional dos Estudantes de Medicina)"
+              text="Em 2019 iniciei minha caminhada na DENEM (Direção Executiva Nacional
+              dos Estudantes de Medicina), no movimento estudantil,
               entidade que me fez entender meu lugar na medicina. Aprendi que saúde
               vai muito além de bem estar social, mental e físico. Aprendi que saúde
-              é justiça social e que para promovê-la, nosso povo necessita de políticas
+              é justiça social e que, para promovê-la, nosso povo necessita de políticas
               públicas fortalecidas. Aprendi que nossa atuação dentro do consultório
-              é insuficiente, e, por isso, a mão que cuida, também deve lutar."
+              é insuficiente, e, por isso, a mão que cuida também deve lutar."
             />
 
             <TimelineItem
               reverseOrder
               img={timelinePhoto2}
-              imgAlt="Ju photo 2"
+              imgAlt="Ju fazendo uma fala de máscara durante a pandemia"
               text="A pandemia foi um momento desafiador para nosso povo, principalmente
-              com o desgoverno promove do negacionismo, desinformação e irresponsabilidade.
-              Foram, pelo menos, 400 mil lutos evitáveis. E, mesmo exaustas, fizemos o
+              com o desgoverno que promovia negacionismo, desinformação e irresponsabilidade.
+              Foram, pelo menos, 400 mil mortes evitáveis. E, mesmo exaustas, fizemos o
               luto virar luta. Ocupamos as ruas pelo Fora Bolsonaro, junto com o movimento
               estudantil de medicina, utilizando máscaras, distanciamento social e álcool 70%."
             />
 
             <TimelineItem
               img={timelinePhoto3}
-              imgAlt="Ju photo 3"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
+              imgAlt="Ju de beca em sua formatura da universidade"
+              text="Foram 6 anos de muito estudo, luta e aprendizado na Universidade de Itaúna.
+              Em 2023 finalmente me formei. Apesar do autoritarismo da reitoria, da ausência
+              de escuta e construção conjunta com os alunos e dos aumentos abusivos de
+              mensalidade, me formei médica. Apesar da falta de estágios e campos de prática,
+              aprendi a cuidar. E hoje, no consultório, curo quando possível, geralmente alivio
+              e acolho sempre. Na contramão do corporativismo, escolho ser humana."
             />
 
             <TimelineItem
               reverseOrder
               img={timelinePhoto4}
-              imgAlt="Ju photo 4"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
+              imgAlt="Ju fazendo uma fala na câmara de vereadores de Itaúna"
+              text="Quando meus caminhos se cruzam com a Juventude Manifesta e com o PSOL,
+              me conecto com a cidade, pois nossa política exige que façamos luta em nosso
+              território, para além dos muros da Universidade. Participei de audiências
+              públicas sobre juventudes, reuniões sobre orçamento, posicionamentos em
+              sessões ordinárias da câmara. Também estive na rua construindo o Fora Bolsonaro,
+              o Tarifa Zero, atos pela democracia após o 08 de janeiro, contra a PEC da
+              bandidagem e pelo fim da escala 6x1."
             />
 
             <TimelineItem
-              img={timelinePhoto5}
-              imgAlt="Ju photo 5"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
+              img={timelinePhoto6}
+              imgAlt="Ju photo 6"
+              text="Após seis meses acompanhando a cena de batalhas da minha cidade, no
+              início de 2023, Ahtay disse “Amiga, falta um MC pra fechar a chave. O Hip
+              Hop tá precisando de você”. E até hoje respondo a esse chamado com disposição.
+              Viajo as cidades do centro-oeste e região metropolitana batalhando, articulando
+              e representando as mulheres da cena. Organizo batalhas em Itaúna, como
+              a 1314RAP e a Manifesta Rap, dos poucos espaços de lazer acessível para
+              a juventude periférica."
             />
 
             <TimelineItem
               reverseOrder
-              img={timelinePhoto6}
-              imgAlt="Ju photo 6"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
-            />
-
-            <TimelineItem
+              img={timelinePhoto5}
+              imgAlt='Ju em sua candidatura de 2024 defendendo o "Tarifa Zero Já"'
+              text="Após dois anos de mobilizações antifascistas e vigilância democrática
+              durante o período Bolsonaro e dois anos de reconstrução do país a partir de
+              políticas públicas durante Lula 3, em 2024 o PSOL Itaúna tinha o desafio de
+              apresentar nossos acúmulos nas eleições municipais. Apresentamos candidaturas
+              que defenderam o tarifa zero, o fomento da cultura e o preparo da nossa cidade
+              para receber a crise climática."
               suppressMarginBottom
-              img={timelinePhoto7}
-              imgAlt="Ju photo 7"
-              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                amet, consectetur adipiscing elit."
             />
           </div>
         </DialogContent>
