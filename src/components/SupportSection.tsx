95fe3b178c421a9d7e926f5ba5554488a2c6ebf3
Valmir Júnior <valmirgmj@gmail.com>
2026-08-11 23:39:50 -0300

    replace synthwave landscape background image with texture paper in SupportSection and update heading style

--- src/components/SupportSection.tsx
+++ src/components/SupportSection.tsx
@@ -1,4 +1,4 @@
-import backgroundImage from "@/assets/background_media/synthwave_landscape.jpg";
+import backgroundImage from "@/assets/background_media/texture_paper.jpg";
 import SupportForm from "./shared/SupportForm";
 
 export default function SupportSection() {
@@ -8,13 +8,13 @@ export default function SupportSection() {
       className="relative w-full overflow-hidden py-20 md:py-32"
       aria-label="Seção Formulário de Apoio"
       style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)), url(${backgroundImage})`,
+        backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
       <div className="container mx-auto max-w-xl px-4 md:max-w-2xl">
-        <h3 className="mb-12 text-center text-white">COLA COM A GENTE!</h3>
+        <h2 className="mb-12 text-center text-black">COLA COM A GENTE!</h2>
 
         <div
           style={{
