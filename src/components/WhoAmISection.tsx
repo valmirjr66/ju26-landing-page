e5f6cb6835b3a45cd10818b24592191b63f7ae5a
Valmir Júnior <valmirgmj@gmail.com>
2026-08-24 19:05:12 -0300

    update WhoAmISection component: add new timeline images and enhance content descriptions for improved storytelling

--- src/components/WhoAmISection.tsx
+++ src/components/WhoAmISection.tsx
@@ -5,6 +5,9 @@ import timelinePhoto3 from "@/assets/photos/timeline_3.jpg";
 import timelinePhoto4 from "@/assets/photos/timeline_4.jpg";
 import timelinePhoto5 from "@/assets/photos/timeline_5.jpg";
 import timelinePhoto6 from "@/assets/photos/timeline_6.jpg";
+import timelinePhoto7 from "@/assets/photos/timeline_7.jpg";
+import timelinePhoto8 from "@/assets/photos/timeline_8.jpg";
+import timelinePhoto9 from "@/assets/photos/timeline_9.jpg";
 import {
   Dialog,
   DialogContent,
@@ -134,7 +137,7 @@ export default function WhoAmISection() {
             <TimelineItem
               reverseOrder
               img={timelinePhoto2}
-              imgAlt="Ju fazendo uma fala de máscara durante a pandemia"
+              imgAlt="Ju em um protesto de máscara durante a pandemia"
               text="A pandemia foi um momento desafiador para nosso povo, principalmente
               com o desgoverno que promovia negacionismo, desinformação e irresponsabilidade.
               Foram, pelo menos, 400 mil mortes evitáveis. E, mesmo exaustas, fizemos o
@@ -144,6 +147,18 @@ export default function WhoAmISection() {
 
             <TimelineItem
               img={timelinePhoto3}
+              imgAlt="Ju fazendo uma fala em um congresso estudantil"
+              text="Participei de alguns congressos estudantis: dois CONUNEs, dois CONUEEs,
+              dois CONUBES… Uns ainda como estudante, outros já formada, como dirigente.
+              Nesses espaços temos a oportunidade de (re) encontrar a Juventude Manifesta
+              que não para de lutar por todo o Brasil, nos reenergizando, trazendo sede de
+              luta e sentido de vida. Nesses eventos, já fiz de tudo: cuidei da saúde das
+              manifesters, articulei, agitei, representei, falei, dialoguei, gritei e voltei
+              pra casa sem voz e com muita vontade."
+            />
+
+            <TimelineItem
+              img={timelinePhoto4}
               imgAlt="Ju de beca em sua formatura da universidade"
               text="Foram 6 anos de muito estudo, luta e aprendizado na Universidade de Itaúna.
               Em 2023 finalmente me formei. Apesar do autoritarismo da reitoria, da ausência
@@ -155,8 +170,8 @@ export default function WhoAmISection() {
 
             <TimelineItem
               reverseOrder
-              img={timelinePhoto4}
-              imgAlt="Ju fazendo uma fala na câmara de vereadores de Itaúna"
+              img={timelinePhoto5}
+              imgAlt="Ju com outras militantes do PSOL segundo uma bandeira do partido"
               text="Quando meus caminhos se cruzam com a Juventude Manifesta e com o PSOL,
               me conecto com a cidade, pois nossa política exige que façamos luta em nosso
               território, para além dos muros da Universidade. Participei de audiências
@@ -167,8 +182,20 @@ export default function WhoAmISection() {
             />
 
             <TimelineItem
+              reverseOrder
               img={timelinePhoto6}
-              imgAlt="Ju photo 6"
+              imgAlt='Ju em sua candidatura de 2024 defendendo o "Tarifa Zero Já"'
+              text="Após dois anos de mobilizações antifascistas e vigilância democrática
+              durante o período Bolsonaro e dois anos de reconstrução do país a partir de
+              políticas públicas durante Lula 3, em 2024 o PSOL Itaúna tinha o desafio de
+              apresentar nossos acúmulos nas eleições municipais. Apresentamos candidaturas
+              que defenderam o tarifa zero, o fomento da cultura e o preparo da nossa cidade
+              para receber a crise climática."
+            />
+
+            <TimelineItem
+              img={timelinePhoto7}
+              imgAlt="Ju em uma batalha de rap"
               text="Após seis meses acompanhando a cena de batalhas da minha cidade, no
               início de 2023, Ahtay disse “Amiga, falta um MC pra fechar a chave. O Hip
               Hop tá precisando de você”. E até hoje respondo a esse chamado com disposição.
@@ -179,15 +206,28 @@ export default function WhoAmISection() {
             />
 
             <TimelineItem
-              reverseOrder
-              img={timelinePhoto5}
-              imgAlt='Ju em sua candidatura de 2024 defendendo o "Tarifa Zero Já"'
-              text="Após dois anos de mobilizações antifascistas e vigilância democrática
-              durante o período Bolsonaro e dois anos de reconstrução do país a partir de
-              políticas públicas durante Lula 3, em 2024 o PSOL Itaúna tinha o desafio de
-              apresentar nossos acúmulos nas eleições municipais. Apresentamos candidaturas
-              que defenderam o tarifa zero, o fomento da cultura e o preparo da nossa cidade
-              para receber a crise climática."
+              img={timelinePhoto8}
+              imgAlt="Registro de uma das primeiras edições da Manifesta Rap"
+              text="Em 2025 a Juventude Manifesta seguiu firme e cada vez mais forte,
+              construindo com o hip hop. Percebemos uma grande necessidade de conversas na cena,
+              tanto sobre nossas relações, quanto sobre a sociedade; para melhorar nossa vida e
+              nossa rima. Assim, nasce a Manifesta Rap em 2025, uma batalha de conhecimento, com
+              temas escolhidos pela plateia, sempre precedida por um papo reto. Já conversamos
+              sobre “o que é ser hip hop?”, “o papel dos homens no combate à violência”,
+              “competitividade tóxica”, “o embranquecimento do Brasil” e
+              “os desafios do povo latinoamericano”."
+            />
+
+            <TimelineItem
+              img={timelinePhoto9}
+              imgAlt="Registro da primeira edição da Manifesta 360º"
+              text="A Manifesta iniciou o ano de 2026 com tudo, e em fevereiro já propôs
+              formações políticas e rap. Como nossa juventude está precarizada na escala 6x1,
+              percebemos que o domingo era o melhor dia pra nossa galera participar.
+              E a partir dessa leitura da realidade, surge a Manifesta 360º, um evento
+              produzido pela Juventude Manifesta que reúne todos os formatos que fazemos.
+              Todo mês fazemos uma edição, um entardecer de lazer em praça pública com oficinas,
+              formações e muito rap."
               suppressMarginBottom
             />
           </div>
