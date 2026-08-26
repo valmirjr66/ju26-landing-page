ba76d64d71648b998cacf18862f5ce205907de16
Valmir Júnior <valmirgmj@gmail.com>
2026-08-26 19:16:42 -0300

    update WhoAmISection: replace background image with a new texture and add a background color for improved visual appeal

--- src/components/WhoAmISection.tsx
+++ src/components/WhoAmISection.tsx
@@ -1,4 +1,4 @@
-import backgroundImage from "@/assets/background_media/texture_paper.jpg";
+import backgroundImage from "@/assets/background_media/texture_grunge.png";
 import timelinePhoto1 from "@/assets/photos/timeline_1.jpg";
 import timelinePhoto2 from "@/assets/photos/timeline_2.jpg";
 import timelinePhoto3 from "@/assets/photos/timeline_3.jpg";
@@ -56,6 +56,7 @@ export default function WhoAmISection() {
         style={{
           backgroundImage: `url(${backgroundImage})`,
           backgroundSize: "cover",
+          backgroundColor: "#32776b",
         }}
       >
         <div className="container mx-auto max-w-2xl px-4">
