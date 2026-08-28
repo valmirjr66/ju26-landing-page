cdd864c672c50c196ce8ab5f34ddc834aeb8c7fc
Valmir Júnior <valmirgmj@gmail.com>
2026-08-28 02:52:55 -0300

    update DialogTitle: increase font size from text-lg to text-2xl for improved visibility

--- src/components/shared/dialog.tsx
+++ src/components/shared/dialog.tsx
@@ -135,7 +135,7 @@ function DialogTitle({
   return (
     <DialogPrimitive.Title
       data-slot="dialog-title"
-      className={cn("text-lg leading-none font-semibold", className)}
+      className={cn("text-2xl leading-none font-semibold", className)}
       {...props}
     />
   );
